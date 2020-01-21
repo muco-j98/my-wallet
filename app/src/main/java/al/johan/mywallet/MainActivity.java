941db41c9fadd4cdf01a39f58e98a1f493bb3313
Johan456 <johanmuco98@gmail.com>
2020-01-21 11:58:45 +0200

    Total amount from initial screen

--- app/src/main/java/al/johan/mywallet/MainActivity.java
+++ app/src/main/java/al/johan/mywallet/MainActivity.java
@@ -1,12 +1,16 @@
 package al.johan.mywallet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -22,14 +26,24 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CreateTransactionDialog.CreateTransactionDialogListener {
     private TransactionViewModel transactionViewModel;
-    double totalAmount = 0;
+    private TextView tvTotalAmount;
+    double totalAmount, initialAmount;
+    private final int REQUEST_CODE = 1;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TOTAL_AMOUNT = "totalAmount";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tVTotalAmount = findViewById(R.id.tvTotalAmount);
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
+        tvTotalAmount = findViewById(R.id.tvTotalAmount);
+
+        if (firstStart) {
+            showInitialActivity();
+        }
 
         FloatingActionButton btnAddTransaction = findViewById(R.id.btnAddTransaction);
         btnAddTransaction.setOnClickListener(new View.OnClickListener() {
@@ -51,13 +65,37 @@ public class MainActivity extends AppCompatActivity implements CreateTransaction
             @Override
             public void onChanged(List<Transaction> transactions) {
                 adapter.setTransactions(transactions);
-
-                for(int i = 0; i < transactions.size(); i++) {
-                    totalAmount += transactions.get(i).getAmount();
-                }
-                tVTotalAmount.setText(String.valueOf(totalAmount));
+                tvTotalAmount.setText(String.valueOf(calculateTotal(transactions)));
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                Transaction currentTransaction = adapter.getTransactionAt(viewHolder.getAdapterPosition());
+                transactionViewModel.delete(currentTransaction);
+                tvTotalAmount.setText(String.valueOf(updateTotalAfterDelete(currentTransaction)));
+                Toast.makeText(MainActivity.this, "Transaction Deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                initialAmount = data.getDoubleExtra("initialAmount", 0);
+                saveData();
+                Toast.makeText(this, "value is + " + initialAmount, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     private void openDialog() {
@@ -75,5 +113,40 @@ public class MainActivity extends AppCompatActivity implements CreateTransaction
         transactionViewModel.insert(transaction);
 
         Toast.makeText(this, "Transaction added!", Toast.LENGTH_SHORT).show();
+    }
+
+    public double calculateTotal(List<Transaction> transactions) {
+        totalAmount = initialAmount;
+        for(int i = 0; i < transactions.size(); i++) {
+            totalAmount += transactions.get(i).getAmount();
+        }
+        return totalAmount;
+    }
+
+    public double updateTotalAfterDelete(Transaction transaction) {
+        if(transaction.getAmount() > 0) {
+            totalAmount -= transaction.getAmount();
+        } else {
+            totalAmount += transaction.getAmount();
+        }
+        return totalAmount;
+    }
+
+    private void showInitialActivity() {
+        Intent intent = new Intent(this, InitialActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
+    }
+
+    //in progress
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putFloat(TOTAL_AMOUNT, Float.parseFloat(tvTotalAmount.getText().toString()));
     }
 }
