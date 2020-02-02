df74c162f4c4e76baa0af4caa7c4c594d1aaaa8a
Johan456 <johanmuco98@gmail.com>
2020-02-02 13:30:06 +0200

    Added splash screen

---
+++ app/src/main/java/al/johan/mywallet/SplashScreen.java
@@ -0,0 +1,17 @@
+package al.johan.mywallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+};
